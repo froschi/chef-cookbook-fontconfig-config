dd7d15607fa0a2f4d29bf83dddd78d446aefff3f
Thorsten Fischer <thorsten@froschi.org>
2012-06-08 23:43:13 +0200

    Amended for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,7 @@
 packages = Array.new
 
 case node[:lsb][:codename]
-when "lucid"
-  packages |= %w/
-    fontconfig-config
-  /
-when "precise"
+when "lucid", "precise"
   packages |= %w/
     fontconfig-config
   /
